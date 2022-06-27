b5e8e98650a11635c8ae8c5bebc6300832a793e5
Renan Fonseca <renanfonseca88@gmail.com>
2022-06-27 16:01:01 -0300

    adicionando tela

---
+++ uPrincipal.pas
@@ -0,0 +1,30 @@
+unit uPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm15 = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    edtNome: TEdit;
+    edtIdade: TEdit;
+    Button1: TButton;
+    Memo1: TMemo;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form15: TForm15;
+
+implementation
+
+{$R *.dfm}
+
+end.
